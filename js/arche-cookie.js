d7c1aeceb1c665e6b3fa8bdd4e59823f89536651
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2025-05-09 15:21:45 +0200

    cart developments

--- js/arche-cookie.js
+++ js/arche-cookie.js
@@ -1,57 +1,7 @@
 jQuery(function ($) {
     $.noConflict();
     "use strict";
-
-
-
-    /**
-     * Set the cookie
-     * @param {type} cname
-     * @param {type} cvalue
-     * @param {type} exdays
-     * @returns {undefined}
-     */
-    window.setCartCookie2 = function (cname, cvalue, exdays) {
-        //function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires=" + d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/; SameSite=None; Secure";
-    }
-
-    /**
-     * Remove cookie
-     * @param {type} cname
-     * @returns {undefined}
-     */
-    window.removeCartCookie2 = function (cname) {
-        //function removeCookie(cname) {
-        document.cookie = cname + '=; Max-Age=-99999999;';
-    }
-
-    /**
-     * Get the cookie
-     * @param {type} cname
-     * @returns {String}
-     */
-    window.getCartCookie2 = function (cname) {
-        //function getCookie(cname) {
-        var name = cname + "=";
-        var decodedCookie = decodeURIComponent(document.cookie);
-        var ca = decodedCookie.split(';');
-        for (var i = 0; i < ca.length; i++) {
-            var c = ca[i];
-            while (c.charAt(0) == ' ') {
-                c = c.substring(1);
-            }
-            if (c.indexOf(name) == 0) {
-                return c.substring(name.length, c.length);
-            }
-        }
-        return "";
-    }
-
-
+    
     window.setCartCookie = function (data) {
         const json = JSON.stringify(data);
         document.cookie = "cart_items=" + json + "; path=/; max-age=" + (30 * 24 * 60 * 60);
@@ -71,13 +21,11 @@ jQuery(function ($) {
     }
 
     window.addCartItem = function (id, itemData) {
-
         const cart = getCartCookie();
         cart[id] = itemData;
         setCartCookie(cart);
     }
     window.removeCartItem = function (id) {
-
         const cart = getCartCookie();
         delete cart[id];
         setCartCookie(cart);
--- js/cart.js
+++ js/cart.js
@@ -2,11 +2,27 @@ jQuery(function ($) {
     $.noConflict();
     "use strict";
 
+    var cartTable;
+
     $(document).ready(function () {
         updateCartHeader();
         displayCartTable();
     });
 
+    function reloadCartTable() {
+        const rawCart = window.getCookieByName('cart_items');
+        const cart = rawCart ? JSON.parse(rawCart) : {};
+
+        const cartArray = Object.entries(cart).map(([id, item]) => ({
+                id,
+                title: item.title,
+                accessres: item.accessres,
+                type: item.type,
+                size: 0
+            }));
+
+        cartTable.clear().rows.add(cartArray).draw();
+    }
 
     function updateCartHeader() {
         const rawCart = window.getCookieByName('cart_items');
@@ -30,95 +46,75 @@ jQuery(function ($) {
                 size: 0
             }));
 
-        console.log("display: ");
-        console.log(cartArray);
 
-        $('#cartTable').DataTable({
-             columnDefs: [
+        cartTable = $('#cartTable').DataTable({
+            columnDefs: [
                 {targets: [0], orderable: false}  // Disable ordering on columns 0 and 2
             ],
             data: cartArray,
             columns: [
                 {data: 'downloadAll', title: '<input type="checkbox" id="check_all_cart_items">', render: function (data, type, row, meta) {
-                        return '<a href="' + row.id + '">' + row.id + '</a>';
+                        return '<input type="checkbox" class="check_cart_item" data-id="'+row.id+'">';
                     }
                 },
                 {data: 'title'},
                 {data: 'size'},
                 {data: 'accessres'},
-                 {data: 'type'},
+                {data: 'type'},
                 {data: 'status', title: Drupal.t('Status'), render: function (data, type, row, meta) {
-                        return 'status text';
+                        return '<div class="downloadStatus dlres-'+row.id+'"></div>';
                     }
                 },
                 {data: 'download', title: Drupal.t('Download'), render: function (data, type, row, meta) {
-                        return '<a href="#" id="download-cart-element" class="btn btn-arche-green" data-id="'+row.id+'">'+ Drupal.t("Download")+'</a> ';
+                        return '<a href="#" id="" class="btn btn-arche-green download-cart-element" data-id="' + row.id + '">' + Drupal.t("Download") + '</a> ';
                     }
                 },
                 {data: 'delete', title: Drupal.t('Delete'), render: function (data, type, row, meta) {
-                        return '<a href="#" id="delete-cart-element" class="btn btn-arche-red" data-id="'+row.id+'">'+ Drupal.t("Delete")+'</a> ';
+                        return '<a href="#" id="" class="btn btn-arche-red delete-cart-element" data-id="' + row.id + '">' + Drupal.t("Delete") + '</a> ';
                     }
                 }
-                
+
             ],
         });
-/*
-        var table = new DataTable('#cartTable', {
-            paging: true,
-            searching: true,
-            searchDelay: 500,
-            lengthChange: false,
-            pageLength: 10,
-            processing: true,
-            deferRender: true,
-            columnDefs: [
-                {targets: [2], orderable: false}  // Disable ordering on columns 0 and 2
-            ],
-            bInfo: false, // Hide table information
-            language: {
-                processing: "<img src='/browser/themes/contrib/arche-theme-bs/images/arche_logo_flip_47px.gif' />",
-                url: datatableLanguage
-            },
-            serverSide: true,
-            serverMethod: "post",
-            ajax: {
-                url: "/browser/api/isPartOfDT/" + resId + "/" + drupalSettings.arche_core_gui.gui_lang,
-                timeout: 10000,
-                complete: function (response) {
-                    $('.loading-indicator.loading-indicator-ispartof').addClass('d-none');
-                    $('.row.ispartof-table-div').removeClass('d-none');
-                    if (response === undefined) {
-                        $('.row.ispartof-table-div').hide();
-                        return;
-                    }
-                },
-                error: function (xhr, status, error) {
-                    console.log("ERROR" + error);
-                    $('.row.ispartof-table-div').hide();
-                    $('.loading-indicator.loading-indicator-ispartof').addClass('d-none');
-                }
-            },
-            columns: [
-                {data: 'acdhid', visible: false},
-                {data: 'id', visible: false},
-
-                {data: 'property', visible: false},
-                {data: 'title', title: Drupal.t('Entity'), render: function (data, type, row, meta) {
-                        return '<a href="' + row.acdhid + '">' + row.title + '</a>';
-                    }
-                },
-                {data: 'type', visible: false}
-            ],
-            fnDrawCallback: function () {
-            }
-        });*/
     }
 
+    function startDownloadAndTrack(url, id) {
+        $('.downloadStatus.dlres-'+id).text('Downloading...');
 
+        // Start download
+        $('#downloadFrame').attr('src', url);
+
+        // Optionally detect when the iframe has finished loading
+        $('#downloadFrame').on('load', function () {
+            $('.downloadStatus.dlres-'+id).text('Download ready.');
+        });
+    }
+
+    $(document).delegate(".download-cart-element", "click", function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        console.log(id);
+
+        var baseApi = drupalSettings.arche_core_gui.apiUrl;
+        var resourceUrl = baseApi + id;
+        console.log(drupalSettings.arche_core_gui);
+        console.log(resourceUrl);
+        startDownloadAndTrack(resourceUrl, id);
+
+    });
+
+    $(document).delegate(".delete-cart-element", "click", function (e) {
+        e.preventDefault();
+        var id = $(this).data('id');
+        console.log(id);
+        window.removeCartItem(id);
+        reloadCartTable();
+
+    });
 
     $(document).delegate("#add-resource-cart", "click", function (e) {
         e.preventDefault();
-        console.log("clicked");
+
         var resId = $('#resId').val();
         var title = $('#resource-main-title').val();
         var accessRes = $('#resource-access').val();
