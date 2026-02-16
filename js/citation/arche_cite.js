c3e212f518bcd4a9ccf03fc8d0c4f6578d51aeca
Norbert Czirjak <norbert.czirjak@oeaw.ac.at>
2026-02-16 14:16:52 +0100

    citation.js removed, new citation endpoint added

--- js/citation/arche_cite.js
+++ js/citation/arche_cite.js
@@ -2,238 +2,199 @@ jQuery(function ($) {
     $.noConflict();
     "use strict";
 
-
-    window.Cite = require('citation-js');
-
-    ///////////// CITE ////////////////////
-    /**
-     * Generate the cite tab header
-     * @param string type
-     * @param string first
-     * @param string typeid -> id for the handle event
-     * @returns string
-     */
-    function createCiteTab(type, typeid) {
-        $('#cite-dropdown').append($('<option></option>').attr('value', typeid.toLowerCase()).text(type.toUpperCase()));
-    }
-
-    /**
-     * Generate the cite block content
-     * @param string data
-     * @param string typeid -> id for the handle event
-     * @param string first
-     * @returns string
-     */
-    function createCiteContent(data, typeid, first, json = false) {
-        var selected = 'selected';
-        if (!first) {
-            selected = 'hidden';
-        }
-        var prewrap = "white-space: unset;";
-        if (json) {
-            prewrap = "white-space: pre-wrap;"
-        }
-        var html = "<span class='cite-content " + selected + "' style='" + prewrap + "' id='highlight-" + typeid.toLowerCase() + "'>" + data + "</span>";
-        $('#cite-content-figure').append(html);
-
-    }
-
-    function displayHarvard(cite, apa_loaded) {
-        var opt = {
-            format: 'string'
-        };
-        opt.type = 'html';
-        opt.style = 'citation-harvard1';
-        opt.lang = 'en-US';
-        createCiteTab('harvard', 'harvard');
-        createCiteContent(cite.get(opt), 'harvard', apa_loaded);
-    }
-
-    function displayVancouver(cite, apa_loaded) {
-        var opt = {
-            format: 'string'
-        };
-        opt.type = 'html';
-        opt.template = 'vancouver';
-        opt.style = 'citation-vancouver';
-        opt.lang = 'en-US';
-        createCiteTab('vancouver', 'vancouver');
-        createCiteContent(cite.get(opt), 'vancouver', false);
-    }
-
-    function displayJsonCsl(data) {
-        const formattedJson = JSON.stringify(data, null, 2);
-        createCiteTab('json-csl', 'json-csl');
-        createCiteContent(formattedJson, 'json-csl', false, true);
-    }
-
-    function displayBiblatex(url) {
-        $.get(url).done(function (data) {
-            createCiteTab('BiblaTex', 'biblatex');
-            createCiteContent(data, 'BiblaTex', false, true);
-        }).fail(function (xhr) {
-            console.log("Biblatex fetch is not possible!");
-            return;
-        });
-
-    }
-
-    function displayArchetCite(url) {
-        $.get(url).done(function (data) {
-            const formattedJson = JSON.stringify(data, null, 2);
-            createCiteTab('Arche Citation', 'arche-citation');
-            createCiteContent(formattedJson, 'arche-citation', false, true);
-        }).fail(function (xhr) {
-            console.log("Arche Citation fetch is not possible!");
-            return;
-        });
-    }
-
-    /**
-     * Show the CITE block
-     * @returns {undefined}
-     */
     window.showCiteBlock = function () {
-        var url = $('#biblaTexUrl').val();
+        $('#cite-loader').hide();
 
-        if (url) {
-            //url = "https://arche-biblatex.acdh.oeaw.ac.at/?id=https://arche.acdh.oeaw.ac.at/api/1819726&lang=en&format=application%2Fvnd.citationstyles.csl%2Bjson";
-            //console.log(url);
-            //$.get(url).done(function (data) {
-            $.get(url + '&lang=' + drupalSettings.arche_core_gui.gui_lang + '&format=application%2Fvnd.citationstyles.csl%2Bjson').done(function (data) {
-                $('#cite-div').removeClass('hidden');
-                $('#cite-main-div').removeClass('hidden');
-                $('#cite-loader').addClass('hidden');
+        const urlBiblatex = $('#biblaTexUrl').val();
+        const baseUrl =
+                urlBiblatex +
+                '&lang=' +
+                drupalSettings.arche_core_gui.gui_lang +
+                '&format=';
+        const citationEndpoints = {
+            "apa-6th": baseUrl + "apa-6th-edition",
+            "harvard": baseUrl + "harvard-cite-them-right",
+            "vancouver": baseUrl + "nlm-citation-sequence",
+            "arche-citation": baseUrl + "arche-citation-style",
+            "json-csl": baseUrl + encodeURIComponent("application/vnd.citationstyles.csl+json"),
+            "biblatex": baseUrl + encodeURIComponent("application/x-bibtex")
+        };
+        
+        const $contentDiv = $('<div>', {
+            id: 'citation-content',
+            class: 'citation-content'
+        });
 
-                try {
-                    let cite = new Cite(data);
-                    var apa_loaded = true;
-                    let templateName = 'apa-6th';
-                    var template = "";
-                    url_csl_content("/browser/modules/contrib/arche_core_gui/csl/apa-6th-edition.csl")
-                            .done(function (data) {
+        const $controls = $(`
+    <div class="container">
+      <div class="row justify-content-end align-items-start">
+        <div class="col-auto">
+          <div class="cite-selector" id="cite-selector-div"></div>
+          <div id="dropdown-container"></div>
+        </div>
+        <div class="col-auto">
+          <a href="#" class="ms-auto btn btn-arche-blue cite" id="copyCite">Copy</a><br>
+          <span id="copy-cite-btn-confirmation" style="display:none;">Citation information copied!</span>
+        </div>
+      </div>
+    </div>
+  `);
 
-                                template = data;
-                                Cite.CSL.register.addTemplate(templateName, template);
-                                var opt = {
-                                    format: 'string'
-                                };
-                                opt.type = 'html';
-                                opt.style = 'citation-' + templateName;
-                                opt.lang = 'en-US';
-                                createCiteTab('apa 6th', 'apa-6th');
-                                createCiteContent(cite.get(opt), 'apa-6th', false, true);
-                                apa_loaded = false;
-                            }).then(function (d) {
+        const $select = $('<select>', {
+            id: 'cite-dropdown',
+            class: 'btn btn-secondary dropdown-toggle'
+        });
 
-                        let archeTestCslName = 'arche-test';
-                                
-                        if (window.location.href.includes("https://arche-dev.acdh-dev.oeaw.ac.at/browser/")) {
-                            return url_csl_content("/browser/modules/contrib/arche_core_gui/csl/ARCHE_citation-style.csl")
-                                .done(function (archeTestCsl) {
+        const labels = {
+            "apa-6th": "APA 6TH",
+            "harvard": "HARVARD",
+            "vancouver": "VANCOUVER",
+            "arche-citation": "ARCHE",
+            "json-csl": "JSON-CSL",
+            "biblatex": "BIBLATEX"
+        };
 
-                                    Cite.CSL.register.addTemplate(archeTestCslName, archeTestCsl);
+        $.each(citationEndpoints, function (key) {
+            $select.append(
+                    $('<option>', {
+                        value: key,
+                        text: labels[key] || key.toUpperCase()
+                    })
+                    );
+        });
 
-                                    let archeTestOpt = {
-                                        format: 'string',
-                                        type: 'html',
-                                        style: 'citation-Arche-test',
-                                        lang: 'en-US'
-                                    };
+        $controls.find('#dropdown-container').append($select);
+        
+        $('#cite-main-div').empty().append($contentDiv, $controls);
+        
+        function escapeHtml(str) {
+            return String(str)
+                    .replaceAll('&', '&amp;')
+                    .replaceAll('<', '&lt;')
+                    .replaceAll('>', '&gt;')
+                    .replaceAll('"', '&quot;')
+                    .replaceAll("'", '&#039;');
+        }
 
-                                    createCiteTab('arche-test', 'arche-test');
-                                    createCiteContent(cite.get(archeTestOpt), 'arche-test', true);
-                                    apa_loaded = false;
-                                });
-                        }
+        function tryParseJson(text) {
+            if (typeof text !== 'string')
+                return {ok: false};
+            const t = text.trim();
+            if (!(t.startsWith('{') || t.startsWith('[')))
+                return {ok: false};
+            try {
+                return {ok: true, value: JSON.parse(t)};
+            } catch (e) {
+                return {ok: false};
+            }
+        }
 
-                    }).then(function (d) {
+        function renderRaw(textOrObj) {
+            const raw =
+                    typeof textOrObj === 'string' ? textOrObj : JSON.stringify(textOrObj, null, 2);
 
-                       // if (window.location.href.includes("https://arche-dev.acdh-dev.oeaw.ac.at/browser/")) {
-                         //   displayArchetCite(url + '&lang=' + drupalSettings.arche_core_gui.gui_lang + '&format=arche-citation-style');
-                       // }
+            $contentDiv.html('<pre class="citation-raw" style="white-space: pre-wrap;word-break: break-word;overflow-wrap: anywhere;">' + escapeHtml(raw) + '</pre>');
+        }
 
-                        //harvard
-                        displayHarvard(cite, apa_loaded);
+        function setCopyConfirmation(show) {
+            const $c = $('#copy-cite-btn-confirmation');
+            if (show) {
+                $c.stop(true, true).show();
+                setTimeout(() => $c.fadeOut(250), 1200);
+            } else {
+                $c.hide();
+            }
+        }
 
-                        //Vancouver
-                        displayVancouver(cite, apa_loaded);
+        function getCopyText() {
+            const pre = $contentDiv.find('pre')[0];
+            if (pre)
+                return (pre.textContent || '').trim();
+            return ($contentDiv.text() || '').trim();
+        }
 
-                        // url + '&lang=' + drupalSettings.arche_core_gui.gui_lang + '&format=application%2Fvnd.citationstyles.csl%2Bjson'
-                        displayBiblatex(url + '&lang=' + drupalSettings.arche_core_gui.gui_lang + '&format=application%2Fx-bibtex');
-                        //displayBiblatex("https://arche-biblatex.acdh.oeaw.ac.at/?id=https://arche.acdh.oeaw.ac.at/api/1819726&lang=en&format=application%2Fx-bibtex");
-                        displayJsonCsl(data);
-                    });
-                } catch (error) {
-                    console.log("CITE ERROR");
-                    $('#cite-loader').addClass('hidden');
-                    createCiteErrorResponse(error);
-                    return false;
-                }
-
-            }).fail(function (xhr) {
-                console.log("CITE FAIL : " + url + '&lang=' + drupalSettings.arche_core_gui.gui_lang + '&format=application%2Fvnd.citationstyles.csl%2Bjson');
-                $('#cite-loader').addClass('d-none');
-                createCiteErrorResponse(Drupal.t("CITE is not available!"));
+        async function copyToClipboard(text) {
+            if (!text)
                 return false;
-            });
+
+            if (navigator.clipboard && window.isSecureContext) {
+                await navigator.clipboard.writeText(text);
+                return true;
+            }
+
+            const $tmp = $('<textarea>')
+                    .val(text)
+                    .css({position: 'fixed', left: '-9999px', top: '0'})
+                    .appendTo('body');
+
+            $tmp[0].focus();
+            $tmp[0].select();
+            const ok = document.execCommand('copy');
+            $tmp.remove();
+            return ok;
         }
-    }
 
-    /**
-     * Display Cite error message
-     * @param {type} errorText
-     * @returns {undefined}
-     */
-    function createCiteErrorResponse(errorText) {
-        $('#cite-div').removeClass('hidden');
-        $('#cite-loader').addClass('hidden');
-        //stop spinner
-        $('#cite-div').html('<div class="alert alert-danger" role="alert">' + Drupal.t(errorText) + '</>');
-    }
-    window.url_csl_content = function (url) {
-        return $.get(url);
-    }
+        function loadCitation(styleKey) {
+            const endpoint = citationEndpoints[styleKey];
+            if (!endpoint)
+                return;
 
-    /**
-     * Handle the cite content changes on select
-     * @param {type} selectedOption
-     * @returns {undefined}
-     */
-    function handleCiteSelectEvents(selectedOption) {
-        $('#cite-content-figure span').removeClass('selected').addClass('hidden');
-        $('#highlight-' + selectedOption).addClass('selected').removeClass('hidden');
-    }
+            $contentDiv.html("");
+            $('#cite-loader').show();
+            setCopyConfirmation(false);
 
-    $('#cite-dropdown').on('change', function (e) {
-        e.preventDefault();
-        var selectedOption = $(this).val(); // Get the selected option value
-        handleCiteSelectEvents(selectedOption);
-    });
+            $.ajax({
+                url: endpoint,
+                method: 'GET',
+                dataType: 'text',
+                headers: {Accept: '*/*'}
+            })
+                    .done(function (data, _textStatus, jqXHR) {
+                        $('#cite-loader').hide();
+                        const contentType = (jqXHR.getResponseHeader('Content-Type') || '').toLowerCase();
 
-    $(document).delegate("a#copyCite", "click", function (e) {
-        var $tempTextarea = $('<textarea>');
-        // Set the textarea value to the content of the div
-        var contentText = "";
-        if (!$('.cite-content.selected .csl-entry').length || !$('.cite-content.selected .csl-entry').text().trim()) {
-            contentText = $('.cite-content.selected').text();
-        } else {
-            contentText = $('.cite-content.selected .csl-entry').text();
+                        const forceRaw =
+                                styleKey === 'json-csl' ||
+                                styleKey === 'biblatex' ||
+                                contentType.includes('application/json') ||
+                                contentType.includes('+json') ||
+                                contentType.includes('application/x-bibtex') ||
+                                contentType.includes('text/x-bibtex');
+
+                        if (forceRaw) {
+                            const parsed = tryParseJson(data);
+                            renderRaw(parsed.ok ? parsed.value : data);
+                        } else {
+                            $contentDiv.html(data);
+                        }
+                    })
+                    .fail(function (jqXHR) {
+                        $('#cite-loader').hide();
+                        const msg = jqXHR.responseText ? jqXHR.responseText : 'Error loading citation.';
+                        renderRaw(msg);
+                    });
         }
-        $tempTextarea.val(contentText);
-        // Append the textarea to the body
-        $('body').append($tempTextarea);
-        // Select the textarea content
-        $tempTextarea.select();
-        // Copy the selected content to the clipboard
-        document.execCommand('copy');
-        // Remove the temporary textarea
-        $tempTextarea.remove();
-        $('#copy-cite-btn-confirmation').show();
-        setTimeout(function () {
-            $('#copy-cite-btn-confirmation').hide();
-        }, 5000);
-        e.preventDefault();
-    });
+
+        $select.on('change', function () {
+            loadCitation($(this).val());
+        });
+
+        $(document).off('click.citeCopy', '#copyCite').on('click.citeCopy', '#copyCite', async function (e) {
+            e.preventDefault();
+
+            const text = getCopyText();
+            if (!text)
+                return;
+
+            try {
+                await copyToClipboard(text);
+                setCopyConfirmation(true);
+            } catch (err) {
+                setCopyConfirmation(false);
+            }
+        });
+
+        loadCitation($select.val());
+    };
 
 });
